5f9129ef0cebefd89a6aae259c5a7c44daab1494
Zosia Samosia <Zosia@Samosia.pl>
2024-04-22 12:40:24 +0200

    chuj

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
 'use strict';
 
+const menuHam = document.querySelector('nav .material-icons');
+const menu = document.querySelector('.div-menu');
+
+const links = document.querySelectorAll('.div-menu a, .div-menu .material-icons');
+
+links.forEach(elem => {
+    elem.addEventListener('click', () => {
+        menu.classList.add('hide-menu');
+    });
+});
+
+menuHam.addEventListener('click', () => {
+    menu.classList.remove('hide-menu');
+});
+
 const labelAll = document.querySelectorAll('label');
 
 const labelVal = [];
@@ -34,8 +49,6 @@ const getDataFromSrv = async dataFromForm => {
 
         const dataFromSrv = await response.json();
 
-        console.log(dataFromSrv);
-
         return dataFromSrv;
 
     } catch(error) {
@@ -48,6 +61,15 @@ const getDataFromSrv = async dataFromForm => {
 const validateData = e => {
     e.preventDefault();
 
+    const resPlace = document.querySelector('#resultPlace');
+    resPlace.innerText = '';
+    resPlace.classList.remove('alert', 'alert-success');
+
+    labelAll.forEach((elem, i) => {
+        elem.classList.remove('alert', 'alert-danger');
+        elem.innerText = labelVal[i];
+    });
+
     const mail = document.querySelector('#email').value;
     const subject = document.querySelector('#subject').value;
     const message = document.querySelector('#message').value;
@@ -58,7 +80,28 @@ const validateData = e => {
         message
     }
 
-    getDataFromSrv(dataFromForm);
+    getDataFromSrv(dataFromForm)
+    .then(res => {
+        console.log(res);
+
+        if('send' in res) {
+            resPlace.innerText = res.send;
+            resPlace.classList.add('alert', 'alert-success');
+            document.querySelectorAll('input:not(input[type="submit"]), textarea').forEach(elem => {
+                elem.value = '';
+            })
+        } else {
+            if('email' in res) {
+                showError(labelAll[0], res.email);
+            }
+            if('subject' in res) {
+                showError(labelAll[1], res.subject);
+            }
+            if('message' in res) {
+                showError(labelAll[2], res.message);
+            }
+        }
+    });
 
 }
 
